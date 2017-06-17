3169a76e40f819b0451e971b1405081f336061ba
Noah Villagomez <villagomezn19@bishopodowd.org>
2017-06-17 22:20:37 +0000

    added css

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -2,7 +2,33 @@ class TodoController < ApplicationController
 def index
 end
 def show
-    @todo_description = "Make a todo"
-    @todo_pomodoro_estimate = 2
+    todo_i = params[:id]
+    todo_i = todo_i.to_i
+    
+    if todo_i == 1
+        @todo_description = "Fix Bed"
+        @todo_pomodoro_estimate = 1
+    elsif todo_i == 2
+        @todo_description = "Clean Room"
+        @todo_pomodoro_estimate = 1
+    elsif todo_i == 3
+        @todo_description = "Put Dishes Away"
+        @todo_pomodoro_estimate = 1
+    elsif todo_i == 4
+        @todo_description = "Work on Homework"
+        @todo_pomodoro_estimate = 3
+    elsif todo_i == 5
+        @todo_description = "Vacuum"
+        @todo_pomodoro_estimate = 1
+    elsif todo_i == 6
+        @todo_description = "Take out the Garbage"
+        @todo_pomodoro_estimate = 1
+    elsif todo_i == 7
+        @todo_description = "Shower"
+        @todo_pomodoro_estimate = 1
+    elsif todo_i == 8
+        @todo_description = "Put Clothes Away"
+        @todo_pomodoro_estimate = 1
+    end
 end
 end
