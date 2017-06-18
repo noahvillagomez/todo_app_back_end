91d8d8d05eb329f8b6152b684e66e5f110e7a87d
Noah Villagomez <villagomezn19@bishopodowd.org>
2017-06-18 21:52:16 +0000

    wired buttons with proper href

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -15,4 +15,15 @@ def create
     t.save
     redirect_to("/todo/show/#{t.id}")
 end
+def edit
+    todo_i = params[:id]
+    @todo = Todo.find_by_id(todo_i)
+end
+def update
+    t = Todo.find_by_id(params[:id])
+    t.description = params["description"]
+    t.pomodoro_estimate = params["pomodoro_estimate"]
+    t.save
+    redirect_to("/todo/show/#{t.id}")
+end
 end
