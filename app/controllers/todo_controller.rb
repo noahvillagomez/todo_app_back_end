e290cd6c55827df6a2e454a03e96d887129a1d20
Noah Villagomez <villagomezn19@bishopodowd.org>
2017-06-18 20:30:33 +0000

    added new rcav flow/ able to create new todo

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,4 +5,14 @@ def show
     todo_i = params[:id]
     @todo = Todo.find_by_id(todo_i)
 end
+def new
+end
+def create
+    t = Todo.new
+    t.description = params["description"]
+    t.pomodoro_estimate = params["pomodoro_estimate"]
+    t.complete = false
+    t.save
+    redirect_to("/todo/show/#{t.id}")
+end
 end
